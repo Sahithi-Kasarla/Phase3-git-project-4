2b6a202196fbc0e1d8b358417c7e9a79cf1dd0c3
Sahithi-kasarla <sahithikasarla123@gmail.com>
2024-01-04 23:00:16 +0530

    Phase3GitProject4

---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GitProject4.Models
+{
+    public partial class Student
+    {
+        public Student()
+        {
+            Marks = new HashSet<Mark>();
+        }
+
+        public int StudentId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public virtual ICollection<Mark> Marks { get; set; }
+    }
+}
